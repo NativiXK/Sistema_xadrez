935645471886b385662ca25334eabdbd6a77f257
NativiXK <mateuskonkol@gmail.com>
2019-02-07 19:12:48 -0200

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,8 +29,8 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
-	public static void clearScreen () {
+
+	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
@@ -55,25 +55,45 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 
-		System.out.println("  a  b  c  d  e  f  g  h");
+		System.out.println("  a b c d e f g h");
+
+	}
+
+	// imprime o tabuleiro atualizado com as peças em suas posições
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+
+		System.out.println("  a b c d e f g h");
 
 	}
 
 	// verifica se existe uma peça na posição verificada, se existir escreve a peça,
 	// se não escreve um traço
-	private static void printPiece(ChessPiece piece) {
+	private static void printPiece(ChessPiece piece, boolean background) {
+
+		if (background) {
+			System.out.print(ANSI_GREEN_BACKGROUND);
+		}
+
 		if (piece == null) {
-			System.out.print("-  ");
+			System.out.print("-" + ANSI_RESET + " ");
 		} else {
 			if (piece.getColor() == Color.WHITE) {
-				System.out.print(ANSI_WHITE + piece + ANSI_RESET + "  ");
+				System.out.print(ANSI_BLUE + piece + ANSI_RESET + " ");
 			} else {
-				System.out.print(ANSI_YELLOW + piece + ANSI_RESET + "  ");
+				System.out.print(ANSI_RED + piece + ANSI_RESET + " ");
 			}
 		}
 	}
